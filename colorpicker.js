c65852818f1f5e92f47268a483f428b0b0f82c54
Bhavana Chilakalapudi <99738757+20481A0456@users.noreply.github.com>
2024-04-10 20:56:00 +0530

    Add files via upload

---
+++ colorpicker.js
@@ -0,0 +1,22 @@
+let mainDiv = document.getElementById("main-div");
+let colorText = document.getElementById("text");
+function button1()
+{
+    mainDiv.style.backgroundColor="#e0e0e0";
+    colorText.textContent="#e0e0e0"
+}
+function button2()
+{
+    mainDiv.style.backgroundColor="#6fcf97";
+    colorText.textContent="#6fcf97"
+}
+function button3()
+{
+    mainDiv.style.backgroundColor="#56ccf2";
+    colorText.textContent="#56ccf2"
+}
+function button4()
+{
+    mainDiv.style.backgroundColor="#bb6bd9";
+    colorText.textContent="#bb6bd9"
+}
